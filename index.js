0cb8a4a1451e2c1ab6bade37f5c64a2287e961c9
osmansufy <osmansufy20@gmail.com>
2021-04-02 16:42:25 +0600

    envFix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ObjectID = require('mongodb').ObjectID;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
-const port = 5000
+const port =process.env.PORT || 5000
 app.use(cors());
 app.use(bodyParser.json());
 require('dotenv').config()
